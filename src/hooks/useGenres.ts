e851a1850a1715cca3e4d4f09a17d87ccf793295
evh671 <eyalnahom@gmail.com>
2023-09-06 17:51:41 +0300

    refactoring - create a reusable API #2

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -18,13 +18,4 @@ const useGenres = () =>
     initialData: { count: genres.length, results: genres },
   });
 
-// const useGenres = () =>
-//   useQuery({
-//     queryKey: ["genres"],
-//     queryFn: () =>
-//       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
-//     staleTime: 24 * 60 * 60 * 1000, // 24 hours,
-//     initialData: { count: genres.length, results: genres },
-//   });
-
 export default useGenres;
