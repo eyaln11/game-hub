662b37de08aa98bb35ae4cb44a1cde1ff31498e2
evh671 <eyalnahom@gmail.com>
2023-09-07 12:36:57 +0300

    Implement infinite queries

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,15 +2,10 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
+  next?: string | null;
+  prev?: string | null;
   results: T[];
 }
-
-// export default axios.create({
-//   baseURL: "https://api.rawg.io/api",
-//   params: {
-//     key: "587d4c80f9ec4d54ba581672dea0e44a",
-//   },
-// });
 
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
